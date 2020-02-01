54d5c9c59d9f0edc7592318dbbda346357d460ab
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-02 00:34:22 +0530

    build added

---
+++ build/precache-manifest.368ecc11221075b15830a1f4924b31b7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2921f599cf7638755cad0e29da72d1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a43bb995b07b2244f8d0",
+    "url": "/static/css/main.7fdf91d3.chunk.css"
+  },
+  {
+    "revision": "c3821d34178fef0cc873",
+    "url": "/static/js/2.118476a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.118476a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a43bb995b07b2244f8d0",
+    "url": "/static/js/main.41cee50f.chunk.js"
+  },
+  {
+    "revision": "d6bd53d88af7870a0b08",
+    "url": "/static/js/runtime-main.f12a7b72.js"
+  },
+  {
+    "revision": "86d2e9e08435edbd0ea0940b35a77361",
+    "url": "/static/media/card1.86d2e9e0.svg"
+  },
+  {
+    "revision": "ebf7da250eeb5e12d12f1190ec88ed2b",
+    "url": "/static/media/card2.ebf7da25.svg"
+  },
+  {
+    "revision": "7db5d49371ff7f5b43ae3efc62f4309f",
+    "url": "/static/media/card3.7db5d493.svg"
+  },
+  {
+    "revision": "788e004897f158022e68cf9c5eaf9611",
+    "url": "/static/media/card4.788e0048.svg"
+  },
+  {
+    "revision": "a1ada80763808ac101f269dc86b8132c",
+    "url": "/static/media/crsl1.a1ada807.png"
+  },
+  {
+    "revision": "3430c3dfc37d0df05eceead5aeffacf2",
+    "url": "/static/media/crsl2.3430c3df.jpg"
+  },
+  {
+    "revision": "8b0143800be13f99f313de7137ea6d2b",
+    "url": "/static/media/crsl3.8b014380.jpg"
+  },
+  {
+    "revision": "ced26f8478d729c1a558534e44cc0052",
+    "url": "/static/media/crsl4.ced26f84.png"
+  },
+  {
+    "revision": "80ad9da81ae57ebd4b6eca10e8780b6d",
+    "url": "/static/media/logo.80ad9da8.svg"
+  },
+  {
+    "revision": "ba4beea02d5d25cf5f9a97f4fd1116bc",
+    "url": "/static/media/vdo-bg-ball.ba4beea0.svg"
+  },
+  {
+    "revision": "d686466ba07f8e6641dd2c948eb476a3",
+    "url": "/static/media/vdo-bg-wall.d686466b.svg"
+  }
+]);
