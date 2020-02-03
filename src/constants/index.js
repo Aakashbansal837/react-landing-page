2ab0225420364843768cb5b554c5280e748f86b7
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-03 17:21:52 +0530

    new varibles added for svg jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,88 @@
-
 export const aboutDivTexthead = `Mera Mobile, Mera Vidyalaya`;
 export const aboutDivTextDis = `Gyanodaya Godda, an education initiative has been successfully instituted by Godda District Administration, Adani Foundation and Eckovation. The learning process used in the Model involves audio-visual classrooms having animated and contextualized lectures followed by daily assessments. The first interaction with the students happens in the classroom, where they watch the video screencast on the television. The videos are kept super interactive which ensures a good level attentiveness from the students`;
 
 export const heatmapDisc = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`;
+
+export const SvgNames = {
+  ar1: {
+    name: "area 1",
+    data: {
+      villages: 1,
+      schools: 1,
+      teachers: 1,
+      students: 1
+    }
+  },
+  ar2: {
+    name: "area 2",
+    data: {
+      villages: 2,
+      schools: 2,
+      teachers: 2,
+      students: 2
+    }
+  },
+  ar3: {
+    name: "area 3",
+    data: {
+      villages: 3,
+      schools: 3,
+      teachers: 3,
+      students: 3
+    }
+  },
+  ar4: {
+    name: "area 4",
+    data: {
+      villages: 4,
+      schools: 4,
+      teachers: 4,
+      students: 4
+    }
+  },
+  ar5: {
+    name: "area 5",
+    data: {
+      villages: 5,
+      schools: 5,
+      teachers: 5,
+      students: 5
+    }
+  },
+  ar6: {
+    name: "area 6",
+    data: {
+      villages: 6,
+      schools: 6,
+      teachers: 6,
+      students: 6
+    }
+  },
+  ar7: {
+    name: "area 7",
+    data: {
+      villages: 7,
+      schools: 7,
+      teachers: 7,
+      students: 7
+    }
+  },
+  ar8: {
+    name: "area 8",
+    data: {
+      villages: 8,
+      schools: 8,
+      teachers: 8,
+      students: 8
+    }
+  },
+  ar9: {
+    name: "area 9",
+    data: {
+      villages: 9,
+      schools: 9,
+      teachers: 9,
+      students: 9
+    }
+  }
+};
