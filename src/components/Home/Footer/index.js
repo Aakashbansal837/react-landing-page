94ecc36a0d105314ef3343583f8e506253db319c
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-11 18:54:44 +0530

    footer data added

--- src/components/Home/Footer/index.js
+++ src/components/Home/Footer/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../../images/FooterLogo.svg";
+import downloadOption1 from "../../../images/downloadOption1.svg";
+import downloadOption2 from "../../../images/downloadOption2.svg";
 
 const Footer = () => {
   return (
@@ -24,22 +26,89 @@ const Footer = () => {
           <div className="row">
             <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
               <div className="ftr-content-head">COMPANY</div>
-              <div className="ftr-content-data">
-                <div className="coloumn">
-                  <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6">
-                    About Us
+              <div className="coloumn ">
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">About Us</div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">Contact Us</div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">Blogs</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              <div className="ftr-content-head">LATEST BLOGS</div>
+              <div className="coloumn ">
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    Launch of Gyanodaya Coffee Table Book
                   </div>
-                  <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6">
-                    Contact Us
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    Enabling The Rural Youth To Grasp The …
                   </div>
-                  <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6">
-                    Blogs
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    Gyanodaya: New Hopes for Kasturba Gandhi…
                   </div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    Gyanodaya: Eckovation Join Hands with Ada.…
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+              <div className="coloumn ">
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    <img
+                      className="ftr-download-options"
+                      src={downloadOption1}
+                      alt="download option"
+                    />
+                  </div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data">
+                    <img
+                      className="ftr-download-options"
+                      src={downloadOption2}
+                      alt="download option"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+              <div className="coloumn ">
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data1">
+                    125 A, Shahpur Jat, Siri Fort New Delhi- 110049
+                  </div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data1">+91-9266677335</div>
+                </div>
+                <div className=" col-lg-12 col-md-12 col-sm-6 col-xs-6 px-0">
+                  <div className="ftr-content-data1">info@eckovation.com</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="container">
+        <div className="ftr-divider"></div>
       </div>
     </div>
   );
