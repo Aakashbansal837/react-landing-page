dc40d4319c3f88db15185b9ad531833bd181fe3e
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-14 13:06:53 +0530

    lazyload added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,36 +1,37 @@
 import React from "react";
-import Navbar from './Navbar';
-import Corousel from './Corousel';
-import Partners from './Partners';
-import About from './About';
-import Heatmap from './Heatmap';
-import Stride from './Stride';
-import Reports from './Reports';
-import Reflections from './Reflections';
-import News from './News';
-import Gallery from './Gallery';
-import DownloadOptions from './DownloadOptions';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Corousel from "./Corousel";
+import Partners from "./Partners";
+import About from "./About";
+import Heatmap from "./Heatmap";
+import Stride from "./Stride";
+import Reports from "./Reports";
+import Reflections from "./Reflections";
+import News from "./News";
+import Gallery from "./Gallery";
+import DownloadOptions from "./DownloadOptions";
+import Footer from "./Footer";
+import Lazyload from "react-lazyload";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar />
-            <Corousel />
-            <Partners />
-            <About />
-            <Heatmap />
-            <Stride />
-            <Reports />
-            <Reflections />
-            <News />
-            <Gallery />
-            <DownloadOptions />
-            <Footer />
-        </div>
-    );
-
+  return (
+    <div>
+      <Lazyload>
+        <Navbar />
+        <Corousel />
+        <Partners />
+        <About />
+        <Heatmap />
+        <Stride />
+        <Reports />
+        <Reflections />
+        <News />
+        <Gallery />
+        <DownloadOptions />
+        <Footer />
+      </Lazyload>
+    </div>
+  );
 };
 
 export default Home;
-
