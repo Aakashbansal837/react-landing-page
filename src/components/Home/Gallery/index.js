d066d4a351d9ce8bcbe5be9ef439ec8c02d55fe8
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-14 18:31:03 +0530

    gallery added

--- src/components/Home/Gallery/index.js
+++ src/components/Home/Gallery/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const Gallery = () => {
+  const data = [
+    {
+      img: ""
+    }
+  ];
   return (
     <div className="gallery">
       <center className="gallery-header">Gallery</center>
       <div className="container">
         <div className="gallery-main">
           <div className="container">
-            <div className="row">
-              <div className="gallery-data-move-left">
-                <span className="gallery-data-move-triangle"></span>
-              </div>
-              <div className="gallery-data-center"> </div>
-              <div className="gallery-data-move-right">
-                <span className="gallery-data-move-triangle"></span>
+            <div className="gallery-data">
+              <div className="row">
+                <div className="gallery-data-move-left">
+                  <span className="gallery-data-move-triangle"></span>
+                </div>
+                <div className="gallery-data-center"> </div>
+                <div className="gallery-data-move-right">
+                  <span className="gallery-data-move-triangle"></span>
+                </div>
               </div>
             </div>
           </div>
