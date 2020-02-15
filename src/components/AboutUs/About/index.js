84af71ac1e32a9c17917abfc4de89721adb21843
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-16 03:12:06 +0530

    about us about section data images added

--- src/components/AboutUs/About/index.js
+++ src/components/AboutUs/About/index.js
@@ -4,6 +4,9 @@ import {
   aboutDivTexthead,
   AboutUsDiscText
 } from "../../../constants";
+import LeftImg from "../../../images/AboutUS/2.jpg";
+import BottomImg from "../../../images/AboutUS/3.svg";
+
 const About = () => {
   return (
     <div className="abus-abt">
@@ -13,14 +16,22 @@ const About = () => {
           <div className="row">
             <div className="col-sm-12 col-xs-12 col-md-6 col-lg-6">
               <div className="abus-main-data">{AboutUsAboutText}</div>
-              <div className="abus-main-img"></div>
+              <div className="abus-main-img">
+                <img className="d-block w-100" src={LeftImg} />
+              </div>
             </div>
             <div className="col-sm-12 col-xs-12 col-md-6 col-lg-6">
               <div className="abus-right-head">{aboutDivTexthead}</div>
               <div className="abus-right-data">{AboutUsDiscText}</div>
             </div>
             <div className="col-12">
-              <div className="abus-bottom"></div>
+              <div className="abus-bottom">
+                <img
+                  className="d-block w-100"
+                  src={BottomImg}
+                  alt="about img"
+                />
+              </div>
             </div>
           </div>
         </div>
