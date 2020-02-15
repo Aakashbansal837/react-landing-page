471aad5091fafe3dfbe6a06f3edfcba25f565f5b
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-16 03:11:38 +0530

    journey data added

--- src/components/AboutUs/Journey/index.js
+++ src/components/AboutUs/Journey/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Img from "../../../images/AboutUS/4.svg";
 
 const Journey = () => {
-  return <div></div>;
+  return (
+    <div className="journey">
+      <center className="journey-header">Our Journey</center>
+      <div className="container">
+        <div className="journey-data">
+          <div className="row">
+            <div className="col-12">
+              <img className="d-block w-100" src={Img} alt="journey image" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Journey;
