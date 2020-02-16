5046a92f94468c15a603e06a188763a48481e3fe
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-16 21:39:33 +0530

    responsice navbar added

--- src/components/Main/Navbar/index.js
+++ src/components/Main/Navbar/index.js
@@ -1,36 +1,89 @@
 import React from "react";
+import { ReactComponent as NbCollapse } from "../../../images/icons/navbar-collapse.svg";
 import { ReactComponent as Logo } from "../../../images/logo.svg";
 
 const Navbar = () => {
+  const [active, setActive] = React.useState(false);
   return (
     <nav className="nvbr">
-      <div className=" container nvbr-container">
-        <a href="/home">
-          <Logo className="nvbr-logo" />
-        </a>
+      <div className="d-none d-md-block">
+        <div className=" container nvbr-container">
+          <a href="/home">
+            <Logo className="nvbr-logo" />
+          </a>
 
-        <ul className="nvbr-menu">
-          <li>
-            <a href="/home" className="nvbr-link">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/aboutUs" className="nvbr-link">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nvbr-link">
-              Blogs
-            </a>
-          </li>
-          <li>
-            <a href="#" className="btn-main">
-              Log In
-            </a>
-          </li>
-        </ul>
+          <ul className="nvbr-menu">
+            <li>
+              <a href="/home" className="nvbr-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/aboutUs" className="nvbr-link">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nvbr-link">
+                Blogs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="btn-main">
+                Log In
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="d-sm-block d-md-none">
+        <div className="container nvbr-sm">
+          <div className="row">
+            <div className="col-2">
+              <span
+                onClick={() => {
+                  setActive(!active);
+                }}
+              >
+                <NbCollapse className="nvbr-sm-cl" />
+              </span>
+            </div>
+            <div className="col-10">
+              <center>
+                <a href="/home">
+                  <Logo className="nvbr-logo" />
+                </a>
+              </center>
+            </div>
+          </div>
+        </div>
+        <img className="" />
+      </div>
+      <div className={active ? " nvbr-sm-active" : "nvbr-sm-non-active"}>
+        <div className="nvbr-sm-data">
+          <ul className="nvbr-sm-menu">
+            <li>
+              <a href="/home" className="nvbr-sm-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/aboutUs" className="nvbr-sm-link">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nvbr-sm-link">
+                Blogs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nvbr-sm-link">
+                Log In
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
