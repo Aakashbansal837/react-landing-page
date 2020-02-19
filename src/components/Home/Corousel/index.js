21009e74f4784894ccb5afa1679208ea395f457f
aakashbansal837 <aakashbansal837@gmail.com>
2020-02-19 18:42:08 +0530

    caption to corousel added

--- src/components/Home/Corousel/index.js
+++ src/components/Home/Corousel/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-import crsl1 from "../../../images/corousel/crsl1.png";
-import crsl2 from "../../../images/corousel/crsl2.png";
-import crsl3 from "../../../images/corousel/crsl3.png";
-import crsl4 from "../../../images/corousel/crsl4.png";
+import crsl1 from "../../../images/corousel/1.png";
+import crsl2 from "../../../images/corousel/2.png";
+import crsl3 from "../../../images/corousel/3.png";
+import crsl4 from "../../../images/corousel/4.png";
 
 const Corousel = () => {
+  const crslHead = [
+    "GYANODAYA",
+    "GYANODAYA",
+    "GAMIFICATION TOOLS",
+    "GYANODAYA"
+  ];
+  const crslText = [
+    "Bringing quality education to more than 600 schools of Jharkhand",
+    "An exemplary public-private partnership model",
+    "Bringing the gamification factor in academics has made education more exciting",
+    "Bringing transparency and accountability in education system"
+  ];
   return (
     <div
       id="carouselExampleIndicators"
@@ -24,15 +36,31 @@ const Corousel = () => {
       <div className="carousel-inner">
         <div className="carousel-item active">
           <img className="d-flex " src={crsl1} alt="First slide" />
+          <center class="carousel-caption">
+            <p className="carousel-caption-head">{crslHead[0]}</p>
+            <p className="carousel-caption-text">{crslText[0]}</p>
+          </center>
         </div>
         <div className="carousel-item">
           <img className="d-flex " src={crsl2} alt="Second slide" />
+          <center class="carousel-caption">
+            <p className="carousel-caption-head">{crslHead[1]}</p>
+            <p className="carousel-caption-text">{crslText[1]}</p>
+          </center>
         </div>
         <div className="carousel-item">
           <img className="d-flex " src={crsl3} alt="Third slide" />
+          <center class="carousel-caption">
+            <p className="carousel-caption-head">{crslHead[2]}</p>
+            <p className="carousel-caption-text">{crslText[2]}</p>
+          </center>
         </div>
         <div className="carousel-item">
           <img className="d-flex " src={crsl4} alt="fourth slide" />
+          <center class="carousel-caption">
+            <p className="carousel-caption-head">{crslHead[3]}</p>
+            <p className="carousel-caption-text">{crslText[3]}</p>
+          </center>
         </div>
       </div>
       <a
